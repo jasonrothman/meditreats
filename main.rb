62ef28e5f049582628228c374e0b2c3cc535b72d
mager <andrew.mager@gmail.com>
2011-07-11 11:19:26 -0700

    Updated dependencies, model; added Mustache

--- main.rb
+++ main.rb
@@ -1,11 +1,30 @@
+require 'bundler/setup'
 require 'rubygems'     
 require 'sinatra'         
+require 'mongo_mapper'
+require 'mustache/sinatra'
 
 get '/' do        
-  "Hello world!"     
+  @posts = Post.all
 end
 
 get 'add' do
   erb :add
 end
 
+
+
+MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com',10076, :pool_size => 5, :timeout => 5)
+MongoMapper.database = 'meditreats-test'
+MongoMapper.database.authenticate('meditreats','goodtreats')
+
+class Recipe
+  include MongoMapper::Document
+  
+  key :title, String
+  key :instructions, String
+  key :directions, String
+  key :img, String
+
+  timestamps!
+end
